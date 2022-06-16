86a9b6e0c18af0831dc51a9b497788b8d6e6bb41
goutham934 <akkaladevigoutham@gmail.com>
2022-06-16 15:54:55 +0530

    1

--- sugar-cosmetic/src/App.js
+++ sugar-cosmetic/src/App.js
@@ -1,11 +1,9 @@
 import React from "react"
-import { Home } from "./Homepage/Home";
 import { MainRoutes } from "./Routes/MainRoutes";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
       <MainRoutes />
     </div>
   );
