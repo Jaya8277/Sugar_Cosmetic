0dd7842be47a5331b8845b5bf2398e9716f09f8d
ShivamRathore07 <shivamrathoreji07@gmail.com>
2022-06-17 07:45:30 +0530

    Add to Cart Page with Routing

--- sugar-cosmetic/src/Routes/MainRoutes.jsx
+++ sugar-cosmetic/src/Routes/MainRoutes.jsx
@@ -5,12 +5,14 @@ import { Navbar } from "../Navbar/Navbar";
 import { CategoryCard } from '../components/CategoryCard';
 import {DivCard} from "../components/DivCard"
 import { Home } from '../Homepage/Home';
+import { Cart } from '../Cart Page/Cart';
 const MainRoutes = () => {
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Cart" element={<Cart/>} />
         <Route path="/products/:category" element={<CategoryCard />} />
         <Route path="/product/category/:_id" element={<DivCard/>} />
       </Routes>
