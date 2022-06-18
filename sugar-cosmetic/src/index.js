ecaef38730e80dc425fcd059cc50364524b9e442
Shubham Kashyap <skashyap061998@gmail.com>
2022-06-18 01:44:17 -0700

    signin done

--- sugar-cosmetic/src/index.js
+++ sugar-cosmetic/src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./ContextApi/CartStorage";
+import { ChakraProvider } from '@chakra-ui/react'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+     <ChakraProvider>
     <BrowserRouter>
       <CartProvider>
         <App />
       </CartProvider>
     </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>,
 );
